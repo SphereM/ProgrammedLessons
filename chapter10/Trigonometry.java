20132e94a73166478adaee55a6708e3d649cfc38
Max Kuybus <icenova360@outlook.com>
2020-09-15 10:26:41 -0600

    Chapter 10 completed

---
+++ chapter10/Trigonometry.java
@@ -0,0 +1,13 @@
+public class Trigonometry 
+{
+    public static void main(String[] args) 
+    {
+        double sinx = 0.5236;
+        double cosx = 0.5236;
+        double sum;
+        sinx = Math.sin(sinx);
+        cosx = Math.cos(cosx);
+        sum = sinx + cosx;
+        System.out.println("sine: "+sinx+" cosine: "+cosx+" sum of squares: "+sum);
+    }
+}
